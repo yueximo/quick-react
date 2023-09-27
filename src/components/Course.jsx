8a197e5189dfb56e27a4268e71021bea5723688d
YueXi Mo <yueximo2024@u.northwestern.edu>
2023-09-27 18:46:37 -0500

    fetch data

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,12 +1,14 @@
 
 const Course = ({courseInfo}) => (
-    <div className="card h-100 w-100 m-1 p-2">
-        <div className="card-body">
-            <h3 className='card-title'>{courseInfo.term} CS {courseInfo.number}</h3>
-            <p className='card-text'>{courseInfo.title}</p>
-        </div>
-        <div>
-            <p className='card-footer bg-transparent'>{courseInfo.meets}</p>
+    <div className="col-lg-auto h-100 p-3">
+        <div className="card h-100 w-100 m-5 p-2">
+            <div className="card-body">
+                <h3 className='card-title'>{courseInfo.term} CS {courseInfo.number}</h3>
+                <p className='card-text'>{courseInfo.title}</p>
+            </div>
+            <div>
+                <p className='card-footer bg-transparent'>{courseInfo.meets}</p>
+            </div>
         </div>
     </div>
 );
