e0b0fdb6af92ef9f4c086c4714fcf270cef51b87
yueximo <yueximo2024@u.northwestern.edu>
2023-10-14 15:35:58 -0500

    DONE

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,9 +1,12 @@
-import "./Course.css";
-import { hasTimeConflict } from "../utilities/conflict";
-import { Link } from "react-router-dom";
-import { useAuthState } from "../utilities/firebase";
+import './Course.css';
+import { hasTimeConflict } from '../utilities/conflict';
+import { Link } from 'react-router-dom';
+import { useAuthState } from '../utilities/firebase';
+import { useProfile } from '../utilities/profile';
 
 const Course = ({ courseInfo, selectedClasses, toggleSelectedClass }) => {
+  const [profile, profileLoading, profileError] = useProfile();
+
   const hasConflict = hasTimeConflict(selectedClasses, courseInfo);
 
   const getCourseId = (courseInfo) => {
@@ -15,32 +18,32 @@ const Course = ({ courseInfo, selectedClasses, toggleSelectedClass }) => {
   const [user] = useAuthState();
 
   return (
-    <div className="col-lg-auto h-100 p-3">
+    <div className='col-lg-auto h-100 p-3'>
       <div
         className={`course card h-100 w-100 m-5 p-2 ${
-          selectedClasses.includes(courseInfo) ? "selected" : "not-selected"
-        } ${hasConflict ? "conflict" : ""}`}
+          selectedClasses.includes(courseInfo) ? 'selected' : 'not-selected'
+        } ${hasConflict ? 'conflict' : ''}`}
         onClick={() => toggleSelectedClass(courseInfo)}
-        style={{ cursor: "pointer" }}
+        style={{ cursor: 'pointer' }}
       >
-        <div className={"card-body"}>
-          <h3 className="card-title">
+        <div className={'card-body'}>
+          <h3 className='card-title'>
             {courseInfo.term} CS {courseInfo.number}
           </h3>
-          <p className="card-text">{courseInfo.title}</p>
+          <p className='card-text'>{courseInfo.title}</p>
         </div>
-        {user ? (
+        {profile.isAdmin ? (
           <Link
             to={`/course/${getCourseId(courseInfo)}/edit`}
-            style={{ display: "inline-block", width: "fit-content" }}
-            className="m-2"
+            style={{ display: 'inline-block', width: 'fit-content' }}
+            className='m-2'
           >
-            {" "}
-            <button className="btn btn-primary">Edit</button>
+            {' '}
+            <button className='btn btn-primary'>Edit</button>
           </Link>
         ) : null}
         <div>
-          <p className="card-footer bg-transparent">{courseInfo.meets}</p>
+          <p className='card-footer bg-transparent'>{courseInfo.meets}</p>
         </div>
       </div>
     </div>
